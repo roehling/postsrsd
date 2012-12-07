5d26960180df29b4a869897429458554c7fa86de
Timo Röhling <timo.roehling@fkie.fraunhofer.de>
2012-12-07 16:18:09 +0100

    Fix special character escape

--- postsrsd.c
+++ postsrsd.c
@@ -125,10 +125,10 @@ static char* url_encode (char* buf, size_t len, const char *input)
   char *output = buf;
   if (!input || !output || len == 0) return NULL;
   while (*input && --len) {
-    if (isspace(*input)) {
+    if (!isascii(*input) || !isgraph(*input) || *input == '%') {
       if (len <= 2) break;
       *output++ = '%';
-      char2hex(*input, output);
+      char2hex(*input++, output);
       output += 2; len -= 2;
     } else {
       *output++ = *input++;
