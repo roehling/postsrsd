37c6dc7b0d09c908fe1e17e66deb57e009169415
Timo Röhling <timo.roehling@gmx.de>
2013-01-10 18:48:09 +0100

    Pacify C compiler
    This eliminates a warning about a missing return
    statement. The code was correct though, because
    hex2num() is guarded by is_hexdigit().

--- postsrsd.c
+++ postsrsd.c
@@ -85,6 +85,7 @@ static char hex2num (char c)
   if (c >= '0' && c <= '9') return c - '0';
   if (c >= 'a' && c <= 'f') return c - 'a' + 10;
   if (c >= 'A' && c <= 'F') return c - 'A' + 10;
+  return 0;
 }
 
 static char num2hex (char c)
