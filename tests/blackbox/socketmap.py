f2666c5a244654f95570494d09e4d6e82e19c2bb
Timo Röhling <timo@gaussglocke.de>
2023-08-12 15:49:31 +0200

    Make blackbox sockmap test robust against partial reads and writes

--- tests/blackbox/socketmap.py
+++ tests/blackbox/socketmap.py
@@ -26,22 +26,44 @@ import tempfile
 import time
 
 
-def write_netstring(sock, data):
+class SockStream:
+    def __init__(self, sock):
+        self._sock = sock
+        self._rdbuf = b""
+
+    def read(self, size):
+        result = b""
+        remaining = size
+        while remaining > len(self._rdbuf):
+            result += self._rdbuf
+            remaining -= len(self._rdbuf)
+            self._rdbuf = self._sock.recv(4096)
+            if len(self._rdbuf) == 0:
+                raise ConnectionError("no data")
+        result += self._rdbuf[:remaining]
+        self._rdbuf = self._rdbuf[remaining:]
+        return result
+
+    def write(self, data):
+        self._sock.sendall(data)
+
+
+def write_netstring(sock_stream, data):
     data_bytes = data.encode()
-    sock.send(f"{len(data_bytes)}:".encode() + data_bytes + b",")
+    sock_stream.write(f"{len(data_bytes)}:".encode() + data_bytes + b",")
 
 
-def read_netstring(sock):
-    digit = sock.recv(1)
+def read_netstring(sock_stream):
+    digit = sock_stream.read(1)
     data_size = 0
     while digit >= b"0" and digit <= b"9":
         data_size = 10 * data_size + int(digit)
-        digit = sock.recv(1)
+        digit = sock_stream.read(1)
     if digit != b":":
         print("ERR: ':' expected")
         return None
-    data = sock.recv(data_size)
-    comma = sock.recv(1)
+    data = sock_stream.read(data_size)
+    comma = sock_stream.read(1)
     if comma != b",":
         print("ERR: ',' expected")
         return None
@@ -87,10 +109,11 @@ def execute_queries(postsrsd, when, use_database, queries):
         assert st.st_mode & 0o777 == 0o666
         sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM, 0)
         sock.connect(endpoint)
+        sock_stream = SockStream(sock)
         try:
             for nr, query in enumerate(queries, start=1):
-                write_netstring(sock, query[0])
-                result = read_netstring(sock)
+                write_netstring(sock_stream, query[0])
+                result = read_netstring(sock_stream)
                 if result != query[1]:
                     raise AssertionError(
                         f"query[{query[0]}]: FAILED: Expected reply {query[1]!r}, got: {result!r}"
@@ -107,15 +130,16 @@ def execute_death_tests(postsrsd, when, use_database, queries):
                 sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM, 0)
                 sock.settimeout(10)
                 sock.connect(endpoint)
-                sock.send(query)
-                result = read_netstring(sock)
+                sock_stream = SockStream(sock)
+                sock_stream.write(query)
+                result = read_netstring(sock_stream)
                 if result != "PERM Invalid query.":
                     raise AssertionError(
                         f"death_test[{query}]: FAILED: Expected reply 'PERM Invalid query.', got: {result!r}"
                     )
                 try:
-                    write_netstring(sock, "forward test@example.com")
-                    result = read_netstring(sock)
+                    write_netstring(sock_stream, "forward test@example.com")
+                    result = read_netstring(sock_stream)
                     raise AssertionError(
                         f"death_test[{query}]: FAILED: Expected connection closed, got: {result!r}"
                     )
